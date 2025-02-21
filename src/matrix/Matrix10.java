8ced6536f59c072aa076f440fee989dfc930df1b
shohruh <sharipovshohruh0916@gmail.com>
2025-02-21 12:30:46 +0500

    Matrix

---
+++ src/matrix/Matrix10.java
@@ -0,0 +1,34 @@
+package matrix;
+
+import java.util.Scanner;
+
+public class Matrix10 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter m:");
+        int m=scanner.nextInt();
+        System.out.print("Enter n:");
+        int n=scanner.nextInt();
+        int [][] matrix=new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0;j<n; j++){
+                matrix[i][j]=scanner.nextInt();
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(matrix[i][j]+"\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                if(j%2!=0){
+                    System.out.print(matrix[i][j]+"\t");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
