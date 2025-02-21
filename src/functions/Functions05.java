76feeadcaa3f566c47425d7a9e3e0f2f9f9037c9
shohruh <sharipovshohruh0916@gmail.com>
2025-02-21 11:44:16 +0500

    Functions

--- src/functions/Functions05.java
+++ src/functions/Functions05.java
@@ -3,20 +3,29 @@ package functions;
 import java.util.Scanner;
 
 public class Functions05 {
-    public  static  void Triangle(double a, double h){
-        double b=Math.pow((a/2),2)+Math.pow(h,2);
-        double result=a+b+b;
-        System.out.println(result);
+    public  static  void SumRange(int a, int b){
+        int sum=0;
+        if(a<b){
+            for(int i=a; i<b; i++){
+                sum+=i;
+            }
+        }else{
+            System.out.println(0);
+        }
+        System.out.println(sum);
     }
-    public  static  void Triangle(){
+    public  static  void SumRange(){
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter a:");
-        double a=scanner.nextDouble();
-        System.out.print("Enter h:");
-        double h=scanner.nextDouble();
-        Triangle(a,h);
+        int a=scanner.nextInt();
+        System.out.print("Enter b:");
+        int b=scanner.nextInt();
+        System.out.print("Enter c:");
+        int c=scanner.nextInt();
+        SumRange(a,b);
+        SumRange(b,c);
     }
     public static void main(String[] args) {
-        Triangle();
+        SumRange();
     }
 }
