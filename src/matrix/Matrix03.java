b866986ff2008cd7b0b314fd71dcfa3dd0d9b0be
shohruh <sharipovshohruh0916@gmail.com>
2025-02-21 10:19:31 +0500

    Matrix

---
+++ src/matrix/Matrix03.java
@@ -0,0 +1,25 @@
+package matrix;
+
+import java.util.Scanner;
+
+public class Matrix03 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.print("Enter m:");
+        int m=scanner.nextInt();
+        System.out.print("Enter n:");
+        int n=scanner.nextInt();
+        int[][] matrix = new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                matrix[i][j]=i;
+            }
+        }
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                System.out.print(matrix[i][j]+"\t");
+            }
+            System.out.println();
+        }
+    }
+}
