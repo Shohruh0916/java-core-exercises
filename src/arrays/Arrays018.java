02826809471f0511684dd6e8b0c41c4497371fbb
shohruh <sharipovshohruh0916@gmail.com>
2025-02-23 02:06:46 +0500

    String

---
+++ src/arrays/Arrays018.java
@@ -0,0 +1,22 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class Arrays018 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] a = new int[10];
+        for (int i = 0; i < 10; i++) {
+            a[i] = scanner.nextInt();
+        }
+        int lastElement = a[9];
+        int result = 0;
+        for (int i = 0; i < 9; i++) {
+            if (a[i] < lastElement) {
+                result = a[i];
+                break;
+            }
+        }
+        System.out.println(result);
+    }
+}
