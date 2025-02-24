107c8d36c55df2fe65d0a94831d30c149ecb1b62
shohruh <sharipovshohruh0916@gmail.com>
2025-02-24 11:38:49 +0500

    String

--- src/string/String22.java
+++ src/string/String22.java
@@ -4,16 +4,19 @@ package string;
 
 import java.util.Scanner;
 
-public class String21 {
+public class String22 {
     public static void main(String[] args) {
         Scanner scanner=new Scanner(System.in);
         System.out.print("Enter str:");
         String str=scanner.nextLine();
-        for(int i=str.length()-1; i>=0; i--){
+        int sum=0;
+        for(int i=0; i<str.length(); i++){
             char s=str.charAt(i);
             if(Character.isDigit(s)){
-                System.out.print(s+" ");
+                int x=s;
+                sum+=x-'0';
             }
         }
+        System.out.println(sum);
     }
 }
